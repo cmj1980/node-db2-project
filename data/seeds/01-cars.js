6429d062c846b8cd3e6fe191fd812b19db26463f
cmj1980 <dagoclmt@gmail.com>
2022-06-16 04:21:44 -0700

    dep installed and 01-cars.js complete

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,28 @@
 // STRETCH
+const cars = [{
+    vin: '1234567891234',
+    make: 'subaru',
+    model: 'forrester',
+    mileage: 25000,
+    title: 'clean',
+    transmission: 'automatic',
+}, {
+    vin: '1234567891234',
+    make: 'honda',
+    model: 'civic',
+    mileage: 250000,
+    title: 'salvage',
+    
+}, {
+    vin: '1234567891234',
+    make: 'BMW',
+    model: 'M3',
+    mileage: 5000,
+    title: 'clean',
+    transmission: 'manual',
+}, ]
+
+exports.seed = async function (knex) {
+   await knex('cars').truncate()
+   await knex('cars').insert(cars)
+}
