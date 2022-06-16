6f13217a3bc00a8c8df2173d88fc5190003c6ad5
cmj1980 <dagoclmt@gmail.com>
2022-06-16 05:25:19 -0700

    cars router plugged in

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,18 @@
 // DO YOUR MAGIC
+const express = require('express')
+
+const router = express.Router()
+router.get('/', async (req, res, next) => {
+    res.json('get all cars')
+})
+
+router.get('/:id', async (req, res, next) => {
+    res.json(`get all cars with ${req.params.id}`)
+})
+
+router.post('/', async (req, res, next) => {
+    res.json('post new car')
+})
+
+
+module.exports = router
